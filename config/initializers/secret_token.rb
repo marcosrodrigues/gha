eebe1455f650fba33ae80d9b53d72cdb3d12105a
Marcos Rodrigues <marcosaureliorod@gmail.com>
2011-07-15 21:23:10 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gha::Application.config.secret_token = '47ec74e513a1ecd2c9c8dc1ede14691a1efd39c7a63316e634e0df907fd208b6697868e74880337bd4d9e88d29a03a0f43eec90ecfb4549123a3aeb4c0fd599d'
