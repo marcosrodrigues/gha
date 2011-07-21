2aa9056ebad7ec052d29b9878c005196be52172e
Marcos Rodrigues <marcosaureliorod@gmail.com>
2011-07-20 23:58:01 -0300

    inicio validações

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -2,5 +2,9 @@ class Cliente < ActiveRecord::Base
 
   SEXO = %w(Masculino Feminino)
 
+  validates_presence_of :codigo
+  validates_presence_of :nome
+  validates_presence_of :sexo
+
 end
 
--- app/models/contrato.rb
+++ app/models/contrato.rb
@@ -1,3 +1,9 @@
 class Contrato < ActiveRecord::Base
   belongs_to :cliente
+
+  validates_presence_of :codigo
+  validates_associated :cliente
+  validates_presence_of :cliente
+  validates_presence_of :data
 end
+
