d1e866f716ae74b07d2e05e34e17ef12268ace83
Marcos Rodrigues <marcosaureliorod@gmail.com>
2011-07-30 17:01:42 -0300

    ajuste validação

--- config/initializers/field_with_errors.rb
+++ config/initializers/field_with_errors.rb
@@ -1,12 +1,9 @@
-ActionView::Base.field_error_proc = Proc.new { |html_tag, instance|
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
   if html_tag =~ /<(input|textarea|select)/
-    %{#{html_tag}
-      <td>
-        <div class="error-left"></div>
-			  <div class="error-inner">Campo obrigatorio.</div>
-		  </td>}.html_safe
+      %{#{html_tag}<td class="validation-error">&nbsp;
+        #{instance.error_message}</td>}.html_safe
   else
     html_tag
   end
-}
+end
 
