bd55c264aa95b3a47e80dfbb364d071273b0a3f7
Marcos Rodrigues <marcosaureliorod@gmail.com>
2011-07-30 17:44:58 -0300

    validação

--- config/initializers/field_with_errors.rb
+++ config/initializers/field_with_errors.rb
@@ -1,7 +1,6 @@
 ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
   if html_tag =~ /<(input|textarea|select)/
-      %{#{html_tag}<td class="validation-error">&nbsp;
-        #{instance.error_message}</td>}.html_safe
+      %{<div class="errorExplanation">#{html_tag}</div>}.html_safe
   else
     html_tag
   end
